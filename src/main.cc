e3047fed398a6f77a71361f26e0bf9e527cd05a8
hly1204 <hly1204@qq.com>
2025-10-06 21:55:22 +0800

    防止启动多个应用程序

--- src/main.cc
+++ src/main.cc
@@ -2,6 +2,9 @@
 #include <QLibraryInfo>
 #include <QTranslator>
 #include <QString>
+#include <QFile>
+#include <QMessageBox>
+#include <QScopeGuard>
 
 #include "main_window.hh"
 #include "asset_bag.hh"
@@ -17,7 +20,7 @@ int main(int argc, char *argv[])
     qRegisterMetaType<AssetBagData>("AssetBagData");
 
     // NOLINTNEXTLINE(readability-static-accessed-through-instance)
-    app.setApplicationName(u"我的小卡片"_s);
+    app.setApplicationName(u"bilibilicardbrowser"_s);
     // NOLINTNEXTLINE(readability-static-accessed-through-instance)
     app.setApplicationVersion(APPLICATION_VERSION);
 
@@ -30,8 +33,26 @@ int main(int argc, char *argv[])
         qWarning() << "Failed to load translations";
     }
 
+    // NOLINTNEXTLINE(readability-static-accessed-through-instance)
+    QFile lock_file(app.applicationDirPath() % "/" % app.applicationName() % ".lock");
+    if (!lock_file.open(QFile::NewOnly | QFile::Truncate)) {
+        QMessageBox msg_box;
+        msg_box.setIcon(QMessageBox::Warning);
+        msg_box.setWindowTitle(u"警告"_s);
+        msg_box.setText(u"为了防止数据竞争 (Data Race) 问题，目前仅支持同时打开一个应用程序。"_s);
+        msg_box.setDetailedText(
+                u"该警告出现可能因为应用程序没有权限在目录中创建文件或上次未正常关闭。"
+                "若因为上次未正常关闭该应用程序，则手动删除 %1 文件即可"_s.arg(
+                        lock_file.fileName()));
+        msg_box.show();
+        // NOLINTNEXTLINE(readability-static-accessed-through-instance)
+        return app.exec();
+    }
+
     MainWindow win;
     win.show();
+
+    const auto cleanup = qScopeGuard([&]() { lock_file.remove(); });
 
     // NOLINTNEXTLINE(readability-static-accessed-through-instance)
     return app.exec();
